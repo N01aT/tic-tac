942ea0549ea45e585f3498d9d3bfafd25873584f
N01aT <160002198+N01aT@users.noreply.github.com>
2024-02-20 11:41:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let playerSymbol = "X";
+let gameEnded = false
+for (let i = 1; i <= 9; i++) {
+    document.getElementById(i.toString()).addEventListener(
+      "click", 
+      function() {
+        if (this.innerHTML === "" && !gameEnded) {
+            this.innerHTML = playerSymbol;
+            this.classList.add(playerSymbol.toLowerCase());
+
+          }if (playerSymbol === "X")
+  playerSymbol = "O"
+else
+  playerSymbol = "X"
+              
+      }
+    );
+  }
